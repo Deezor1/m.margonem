10f7b5feaeaf1dc759e851dfadee7420892d95f3
Deezor <42243233+Deezor1@users.noreply.github.com>
2018-12-19 15:23:30 +0100

    Add files via upload

---
+++ ripple.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+if(!('ontouchstart' in window)) window.location = 'https://deezor1.github.io/margonem/';
+var addRippleEffect = function (e) {
+  var target = e.target;
+  if (target.tagName.toLowerCase() !== 'div') return false;
+  var rect = target.getBoundingClientRect();
+  var ripple = target.querySelector('.ripple');
+  if (!ripple) {
+      ripple = document.createElement('span');
+      ripple.className = 'ripple';
+      ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
+      target.appendChild(ripple);
+  }
+  ripple.classList.remove('show');
+  var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+  var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+  ripple.style.top = top + 'px';
+  ripple.style.left = left + 'px';
+  ripple.classList.add('show');
+  return false;
+}
+document.addEventListener('click', addRippleEffect, false);
