5c53f03c43d5af319a570cd727e3f7016a1540b9
Deezor <42243233+Deezor1@users.noreply.github.com>
2018-12-19 16:49:21 +0100

    Update ripple.js

--- ripple.js
+++ ripple.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
 if(!('ontouchstart' in window)) window.location = 'https://deezor1.github.io/margonem/';
-var addRippleEffect = function (e) {
-  var target = e.target;
+const addRippleEffect = function (e) {
+  let target = e.target;
   if (target.tagName.toLowerCase() !== 'div') return false;
-  var rect = target.getBoundingClientRect();
-  var ripple = target.querySelector('.ripple');
+  let rect = target.getBoundingClientRect();
+  let ripple = target.querySelector('.ripple');
   if (!ripple) {
       ripple = document.createElement('span');
       ripple.className = 'ripple';
@@ -14,8 +14,8 @@ var addRippleEffect = function (e) {
       target.appendChild(ripple);
   }
   ripple.classList.remove('show');
-  var top = e.touches[0].pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
-  var left = e.touches[0].pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+  let top = e.touches[0].pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+  let left = e.touches[0].pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
   ripple.style.top = top + 'px';
   ripple.style.left = left + 'px';
   ripple.classList.add('show');
